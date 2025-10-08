daa68345f4dee3d004da36de795d9c0dbad19e80
Cathrine <achc.sfi@outlook.com>
2025-10-08 11:38:21 +0200

    first commit

---
+++ js/info.js
@@ -0,0 +1,1 @@
+export const baseUrl = "http://localhost/fake_info";
---
+++ tests/script.test.js
@@ -0,0 +1,42 @@
+app.test.js;
+const request = require("supertest");
+const app = require("./script");
+const { describe } = require("yargs");
+
+//TODO - Testing endpoints
+describe("API routes", () => {
+  test("GET /person", async () => {
+    const res = await request(app).get("/api/hello");
+    expect(res.statusCode).toBe(200);
+    expect(res.body).toEqual({ message: "Hello, world!" });
+  });
+
+  test("POST /api/echo returns the sent body", async () => {
+    const payload = { name: "ChatGPT" };
+    const res = await request(app).post("/api/echo").send(payload);
+    expect(res.statusCode).toBe(201);
+    expect(res.body).toEqual({ youSent: payload });
+  });
+});
+
+testPerson = {
+  CPR: "2109615183",
+  firstName: "Jens S.",
+  lastName: "Paulsen",
+  gender: "male",
+  birthDate: "1961-09-21",
+  address: {
+    street: "\u00e6zZPxl\u00f8fIt\u00d8kXFMhhP\u00f8cnXIcQJ\u00c6oJVQDxwTsBJaJ",
+    number: "466R",
+    floor: "st",
+    door: "z92",
+    postal_code: "6823",
+    town_name: "Ansager",
+  },
+  phoneNumber: "61216361",
+};
+//Testing functionalities
+describe("Testing handlePersonData()", () => {});
+
+//TODO NEGATIVE TESTS
+describe("Error handling", () => {});
